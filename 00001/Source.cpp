d868c43b34d20d5622d9a3d722f7867106b1f9c2
yida-li <gabrielyidali@gmail.com>
2021-07-10 12:32:00 -0400

    refactored repo

--- 00001/Source.cpp
+++ 00001/Source.cpp
@@ -13,7 +13,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* yida = glfwCreateWindow(800, 800, "program 0000", NULL, NULL);
+	GLFWwindow *yida = glfwCreateWindow(800, 800, "program 0000", NULL, NULL);
 	if (yida == NULL)
 	{
 		cout << "Failed to initialize this like a coen 317 microprocessor";
@@ -22,45 +22,43 @@ int main()
 	}
 
 	GLfloat vertices[] = {
-		-0.5f,-0.5f * float(sqrt(3))/3, 0.0f,
-		0.5f,-0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f,0.5f * float(sqrt(3))*2 / 3, 0.0f,
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
 
-		-0.5f / 2, 0.5f * float(sqrt(3))/6 , 0.0f,
-		0.5f/ 2, 0.5f * float(sqrt(3))/6 , 0.0f,
-		0.0f, -0.5f * float (sqrt(3))/3, 0.0f
+		/**/ -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
 
 	};
 
 	GLuint matrix[] = {
 
-
-		0,3,5,3,2,4,5,4,1
+		0, 3, 5, 3, 2, 4, 5, 4, 1
 
 	};
-
-
 
 	glfwMakeContextCurrent(yida);
 
 	gladLoadGL();
 	glViewport(0, 0, 800, 800);
 
-	const char* vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z,1.0);\n"
-		"}\0";
-	const char* fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(0.8f,0.3f,0.02f,1.0f);\n"
-		"}\n\0";
-
-
-
+	const char *vertexShaderSource = "#version 330 core\n"
+									 "layout (location = 0) in vec3 aPos;"
+									 "layout (location = 1) in vec3 aColor;"
+									 "out vec3 vertexColor;"
+									 "void main()"
+									 "{"
+									 "   vertexColor = aColor;"
+									 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+									 "}";
+	const char *fragmentShaderSource = "#version 330 core\n"
+									   "in vec3 vertexColor;"
+									   "out vec4 FragColor;"
+									   "void main()"
+									   "{"
+									   "   FragColor = vec4(0.8f,0.7f,0.02f,4.0f);"
+									   "}";
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -70,7 +68,6 @@ int main()
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
-
 	GLuint shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
@@ -79,16 +76,13 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-
 	// buffers
 
 	GLuint VAO, VBO, EBO;
-	
+
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
-
-
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -97,20 +91,11 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(matrix), matrix, GL_STATIC_DRAW);
 
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-
-
-
-
-
-
-
 
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -119,34 +104,29 @@ int main()
 
 	while (!glfwWindowShouldClose(yida))
 	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClearColor(0.07f, 0.13f, 0.60f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		
+
 		// 1. simple triangle
 		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		
+
 		// 2. 4 triangles
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-		
+
 		glfwSwapBuffers(yida);
 
 		glfwPollEvents();
 	}
-
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
-
-
-
 	glfwDestroyWindow(yida);
 	glfwTerminate();
 
 	return 0;
-
 }
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # COMP371
 
-
 # Requirements
 
 > > IDE : Visual Studio 2019+
@@ -10,3 +9,9 @@
 > > GLFW : Graphics Library Framework
 
 > > Programming Language : C++
+
+# Progress
+
+> > > > > ![til](progress/s2.PNG)
+
+> > > > > ![til](progress/s1.PNG)
